098438ed47ea2553b648574e00640d54f58eb2ea
Nikita <Xenomorfchyma@yandex.ru>
2022-04-30 07:56:23 +1000

    Теперь Start Game работает без камеры

--- inference.py
+++ inference.py
@@ -24,15 +24,16 @@ def main():
 
     # define the window layout
 
-    layout = [[sg.Text('OpenCV Demo', size=(40, 1), justification='center', font='Helvetica 20')],
+    layout = [[sg.Text('RPS Demo', size=(40, 1), justification='center', font='Helvetica 20')],
               [sg.Text("User:"), sg.Image(filename='resourses/gui_images/background.png', key='image', size=(256, 256)),
                sg.Text(text="Computer"),
-               sg.Image(filename='resourses/gui_images/background.png', key='-computerkey-', visible=True, size=(256, 256)),
-               sg.Text(key="pobed")],
+               sg.Image(filename='resourses/gui_images/background.png', key='-computerkey-', visible=True, size=(256, 256))],
               [sg.Button("Start Game", size=(10, 1), font='Helvetica 14'),
                sg.Button('Start Camera', size=(10, 1), font='Helvetica 14'),
                sg.Button('Stop', size=(10, 1), font='Any 14', ),
-               sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
+               sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ],
+              [sg.Text('Сложность:', size=(15, 1), justification='center', font='Helvetica 20'), sg.Text(text='Равная', key='-settings-', font='Helvetica 20',)],
+              [sg.Text(key="pobed", size=(30, 1), justification='center', font='Helvetica 20')]]
     # filename = 'resourses/gui_images/paper.png',
 
     # create the window and show it without the plot
@@ -46,7 +47,7 @@ def main():
     recording = False
     a = 1
 
-    # test_transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor()])
+    # test_transform = transforms.Compose([taransforms.ToPILImage(), transforms.ToTensor()])
     # test_transform(hand_points_image).unsqueeze(0)
     # test_transform(hand_image).unsqueeze(0)
     # Работа с изображением и изображением с точками - ответ от нейронки в виде цифры(сделать)
@@ -63,18 +64,12 @@ def main():
                 recording = True
 
             except ValueError as e:
+                window['pobed'].update('Подключите камеру')
+                recording = False
                 continue
-            a = 0
-            # tmp = np.zeros((1024, 768, 3), np.uint_8)
-            # img[y1:y2, x1:x2] = tmp[y3:y4, x3:x4]
-            # hand_points_image, hand_image, x, y = hand_extractor(frame, width, height)
-
-
-        elif event == 'Start Camera':
-            recording = True
-
-        elif event == 'Stop':
+            # if recording == False:
             RPS.settings = 'y'
+            window['-settings-'].update('Непобедимо')
             window.refresh()
             window['-computerkey-'].update(filename='resourses/gui_images/3.png', size=(256, 256))
             window.refresh()
@@ -88,16 +83,44 @@ def main():
             a = 0
 
             recording = False
+            # tmp = np.zeros((1024, 768, 3), np.uint_8)
+            # img[y1:y2, x1:x2] = tmp[y3:y4, x3:x4]
+            # hand_points_image, hand_image, x, y = hand_extractor(frame, width, height)
+
+
+        elif event == 'Start Camera':
+            recording = True
+
+        elif event == 'Stop':
+            # RPS.settings = 'y'
+            # window['-settings-'].update('Непобедимо')
+            # window.refresh()
+            # window['-computerkey-'].update(filename='resourses/gui_images/3.png', size=(256, 256))
+            # window.refresh()
+            # sleep(1)
+            # window['-computerkey-'].update(filename='resourses/gui_images/2.png', size=(256, 256))
+            # window.refresh()
+            # sleep(1)
+            # window['-computerkey-'].update(filename='resourses/gui_images/1.png', size=(256, 256))
+            # window.refresh()
+            # Event().wait(1.0)
+            # a = 0
+
+            recording = False
 
         if recording:
-            hand_points_image, hand_image, _, _ = hand_extractor(frame, width, height)
-            imgbytes = cv2.imencode('.png', hand_image)[1].tobytes()  # ditto
-            window['image'].update(data=imgbytes)
+            try:
+                hand_points_image, hand_image, _, _ = hand_extractor(frame, width, height)
+                imgbytes = cv2.imencode('.png', hand_image)[1].tobytes()  # ditto
+                window['image'].update(data=imgbytes)
+            except FileNotFoundError:
+                window['pobed'].update('Подключите камеру')
         if a == 0:
             try:
                 # получает значение и сразу ответ
                 RPS.user_action = RPS.get_user_selection()
                 recording = False
+                # вывод картинки с ответом игрока
                 b = RPS.user_action
                 if RPS.user_action == 0:
                     window['image'].update(filename='resourses/gui_images/rock.png', size=(256, 256))
@@ -111,7 +134,7 @@ def main():
                 print(f"Не правильное значение, выберите число между {RPS.range_str}")
                 continue
             RPS.computer_action = RPS.get_computer_selection()
-
+            # вывод картинки с ответом компьютера
             z = int(RPS.determine_winner(RPS.user_action, RPS.computer_action))
             if z == 0:
                 window['-computerkey-'].update(filename='resourses/gui_images/rock.png', size=(256, 256))
